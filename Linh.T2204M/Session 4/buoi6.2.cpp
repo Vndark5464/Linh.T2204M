1ee9872cb90eb4baccb985d3a1fe0295de6ff086
Vndark5464 <105443340+Vndark5464@users.noreply.github.com>
2022-05-23 23:53:34 +0700

    Buoi6

---
+++ Linh.T2204M/Session 4/buoi6.2.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int n;
+	printf("nhap n ");
+	scanf("%d",&n);
+	int arr[n];
+	printf("nhap mang\n");
+	for(int i = 0;i <n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	int sum = 0,dem = 0;
+	for(int i =0;i < n;i++)
+	{
+	if(arr[i]%2 !=0 && i%2 != 0){
+		sum += arr[i];
+		dem++;
+		}}
+printf("tong = %f",(float)sum/dem);
+}
